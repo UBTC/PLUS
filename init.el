90eaa6ae1de49e7a516b4371cdd000d0c1a11f56
M.W <mogeiwang@gmail.com>
2016-02-23 23:21:32 +0800

    Set zh-cn font
    New problem in Emacs 25+???

--- init.el
+++ init.el
@@ -494,18 +494,13 @@
 (load-package-layer theme-packages)
 
 ;; Custom sets
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((lentic-init . lentic-orgel-org-init))))
- '(session-use-package t nil (session)))
+;; Custom sets
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "文泉驿等宽正黑" :foundry "unknown" :slant normal :weight normal :height 128 :width normal))))
  '(window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold))) t))
 
 
